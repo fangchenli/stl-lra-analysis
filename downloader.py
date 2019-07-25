e658f27c51eeacdf844090d5cc3e7d6763a003c6
VirosaLi <fangchen.li@outlook.com>
2019-07-24 23:39:36 -0500

    "init commit"

---
+++ downloader.py
@@ -0,0 +1,61 @@
+from os import path, mkdir, remove
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import googlemaps
+
+base_url = 'https://www.stlouis-mo.gov'
+
+url = 'https://www.stlouis-mo.gov/government/departments/sldc/real-estate/lra-owned-property-full-list.cfm'
+
+api_key = 'AIzaSyCJ-Qnrwu7l4GSYfEsDyTfoL9VZOpj_zxU'
+
+data_dir = 'data'
+
+# create data directory if not exist
+if not path.exists(data_dir):
+    mkdir(data_dir)
+
+gmaps = googlemaps.Client(key=api_key)
+
+# get LRA page
+page = requests.get(url)
+
+# make a soup
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# find all "a" tags with href
+a_tags = soup.find_all('a', href=True)
+
+# download xls files and store to csv format
+for a_tag in a_tags:
+    href = a_tag['href']
+
+    if href.endswith('.xls'):
+
+        # use + to join the path since the href start with a back slash, path.join() doesn't work in this case
+        data_path = base_url + href
+
+        # get the file
+        r = requests.get(data_path, allow_redirects=True)
+
+        base_name = path.basename(data_path)
+
+        file_path = path.join(data_dir, base_name)
+
+        # write the content of the request to local dir
+        with open(file_path, 'wb') as f:
+            f.write(r.content)
+
+        # read the xls file
+        df = pd.read_excel(data_path)
+
+        # change the extension to .csv
+        csv_path = file_path.replace('.xls', '.csv')
+
+        # store the csv file
+        df.to_csv(csv_path, index=False)
+
+        # remove the original xls file
+        remove(file_path)
---
+++ processor.py
@@ -0,0 +1,37 @@
+from os import path, scandir
+
+import pandas as pd
+import googlemaps
+
+
+def get_latlng(client, address):
+
+    address_completion = ', St. Louis, MO, USA'
+    geocode_result = googlemaps.client.geocode(client, address+address_completion)
+
+    if not geocode_result:
+        print("get none result")
+        exit(1)
+    else:
+        return geocode_result[0]['geometry']['location']
+
+
+api_key = 'AIzaSyCJ-Qnrwu7l4GSYfEsDyTfoL9VZOpj_zxU'
+
+# initialize a google map client
+gmaps = googlemaps.Client(key=api_key)
+
+with scandir('data') as it:
+
+    for entry in it:
+
+        data_path = path.join('data', entry.name)
+
+        df = pd.read_csv(data_path)
+
+        # use the google map api to get the lat
+        df['latlng'] = df['Address'].map(lambda addr: get_latlng(gmaps, addr))
+
+        df.to_csv(data_path, index=False)
+
+        print(data_path, 'is finished')
